49e50534d197ac65c59c1135ce1b8c42f472d97d
aligra <77466796+aligra@users.noreply.github.com>
2021-03-10 21:44:08 -0600

    ADD: AddNumbers

---
+++ AddNumbers/AddNumbers.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Opt 
+{
+    class Method
+    {
+        static void Main (string [] args)
+        {
+            Console.WriteLine ("Ingrese dos numeros enteros: ");
+            int a = 0, b = 0;
+
+            a = Int32.Parse(Console.ReadLine());
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine ($"Suma: {AddNumbers (a, b)}");
+        }
+
+        static int AddNumbers (int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
